ea039c50c7ece21fed8548cf72e1e2781d5ab0e3
cchawalit@hotmail.com <cchawalit@hotmail.com>
2024-02-14 00:11:44 +0700

    update

--- shecup_fsms_audit.php
+++ shecup_fsms_audit.php
@@ -1,10 +1,120 @@
 <?php
 session_start();
+include_once("php/database.class.php");
 
-if(!isset($_SESSION['firstname'])){
-  header("Location:login.php");
+if(!isset($_SESSION['lang'])){
+  $_SESSION['lang'] = 'th';
 }
-// print_r($_SESSION);
+
+if(isset($_POST['lang']) && strlen($_POST['lang'])>0){
+  $_SESSION['lang'] = $_POST['lang'];
+
+  if($_POST['lang'] == 'en'){
+    header("location:shecup_fsms_audit.php?id=1");
+  }else{
+    header("location:shecup_fsms_audit.php?id=87");
+  }
+}
+
+
+$db = new Database();
+$db->connect();
+$db->select('shecup_fsms_audit','id, question_no, question',NULL,'parent_id = 0 and question_lang="'.$_SESSION['lang'].'"','id ASC'); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+$res = $db->getResult();
+// echo "<pre>";
+// print_r($res);
+$list_step = "";
+
+for($i=0;$i<count($res);$i++){
+  $list_step .= '<li class="step-item" style="word-wrap: normal;"><a href="'.$_SERVER["PHP_SELF"].'?id='.$res[$i]['id'].'">'.$res[$i]['question'].'</a></li>';
+}
+
+if(isset($_GET['id']) && $_GET['id'] > 0){
+
+  $db->select('shecup_fsms_audit','id, question_no, question',NULL,'id ='.$_GET['id'].'','id ASC'); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+  $res = $db->getResult();
+  $section_id = $res[0]['id'];
+  $section_no = $res[0]['question_no'];
+  $section_question = $res[0]['question'];
+
+// echo "<pre>";
+// print_r($res);
+
+
+  $db->select('shecup_fsms_audit','id, question_no, question,risk_level,score',NULL,'parent_id ='.$_GET['id'].'','id ASC'); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+  $res = $db->getResult();
+
+  $list_question = "";
+
+
+// echo "<pre>";
+// print_r($res);
+
+  for($i=0;$i<count($res);$i++){
+
+    // echo $res[$i]['risk_level'];
+
+
+    if($res[$i]['risk_level'] == "C"){
+      $bgcolor_risk_level = 'bg-red text-primary-fg';
+    }else if($res[$i]['risk_level'] == "M"){
+      $bgcolor_risk_level = 'bg-yellow text-primary-fg';
+    }else{
+      $bgcolor_risk_level = 'bg-muted-lt';
+    }
+
+
+
+
+    $list_question .= '<div class="card-body"><div class="divide-y">
+    <div>
+      <div class="row">
+        <div class="col-auto">
+          <span class="avatar bg-primary text-primary-fg">'.$res[$i]['question_no'].'</span>
+        </div>
+        <div class="col">
+          <div class="text-truncate" style="white-space: normal;">
+            <strong>'.$res[$i]['question'].'</strong>
+          </div>
+        </div>
+        <div class="col-auto align-self-center">
+          <span class="avatar '.$bgcolor_risk_level.'">'.$res[$i]['risk_level'].'</span>
+          <span class="avatar bg-green text-primary-fg">'.$res[$i]['score'].'</span>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <div class="mb-3">
+          <label class="form-label"><strong>Compliance Status</strong></label>
+          <label class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="radios-inline" checked="" />
+            <span class="form-check-label">Non Applicant</span>
+          </label>
+          <label class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="radios-inline" />
+            <span class="form-check-label">Compliance</span>
+          </label>
+          <label class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="radios-inline" />
+            <span class="form-check-label">Non Compliance</span>
+          </label>
+          <label class="form-label"><strong>Findings</strong></label>
+          <textarea class="form-control" rows="3"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    ';
+  }
+
+
+// echo "<pre>";
+// print_r($res);
+
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -20,8 +130,6 @@ if(!isset($_SESSION['firstname'])){
   body{
     font-family: 'Sarabun', sans-serif;
   }
-
-  
 </style>
 </head>
 <body>
@@ -36,7 +144,7 @@ if(!isset($_SESSION['firstname'])){
         <div class="row g-2 align-items-center">
           <div class="col">
             <h3 class="page-title" style="font-size:15px;color:#333333;">
-              Home / SHEDEIN
+               Home / SHECUP / FSMS Audit Verification  
             </h3>
           </div>
         </div>
@@ -44,240 +152,76 @@ if(!isset($_SESSION['firstname'])){
     </div>
     <!-- Page body -->
     <div class="page-body">
-      <!-- <div class="container-xl">
-        <div class="card">
-          <div class="card-body">
-            <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasStart" role="button" aria-controls="offcanvasStart">
-              Toggle start offcanvas
-            </a>
-            <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasEnd" role="button" aria-controls="offcanvasEnd">
-              Toggle end offcanvas
-            </a>
-            <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasTop" role="button" aria-controls="offcanvasTop">
-              Toggle top offcanvas
-            </a>
-            <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasBottom" role="button" aria-controls="offcanvasBottom">
-              Toggle bottom offcanvas
-            </a>
-          </div>
-        </div>
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasStart" aria-labelledby="offcanvasStartLabel">
-          <div class="offcanvas-header">
-            <h2 class="offcanvas-title" id="offcanvasStartLabel">Start offcanvas</h2>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body">
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis repellat sit soluta unde. Aut!
-            </div>
-            <div class="mt-3">
-              <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-                Close offcanvas
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasEnd" aria-labelledby="offcanvasEndLabel">
-          <div class="offcanvas-header">
-            <h2 class="offcanvas-title" id="offcanvasEndLabel">End offcanvas</h2>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body">
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis repellat sit soluta unde. Aut!
-            </div>
-            <div class="mt-3">
-              <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-                Close offcanvas
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-          <div class="offcanvas-header">
-            <h2 class="offcanvas-title" id="offcanvasTopLabel">Top offcanvas</h2>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body">
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis repellat sit soluta unde. Aut!
-            </div>
-            <div class="mt-3">
-              <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-                Close offcanvas
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-          <div class="offcanvas-header">
-            <h2 class="offcanvas-title" id="offcanvasBottomLabel">Bottom offcanvas</h2>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body">
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis repellat sit soluta unde. Aut!
-            </div>
-            <div class="mt-3">
-              <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-                Close offcanvas
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> -->
+          <div class="container-xl">
+            <div class="row row-cards">
 
 
-      <div class="container-xl">
-
-<div class="col-12 fixed-bottom" style="margin-bottom:60px;">
+            <div class="col-lg-4">
                 <div class="card">
                   <div class="card-body">
-                    <ul class="pagination ">
-                      <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                          <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M15 6l-6 6l6 6"></path></svg>
-                          prev
-                        </a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">
-                          next <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 6l6 6l-6 6"></path></svg>
-                        </a>
-                      </li>
-                    </ul>
-                    <ul class="pagination ">
-                      <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                          <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M15 6l-6 6l6 6"></path></svg>
-                        </a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">4</a></li>
-                      <li class="page-item"><a class="page-link" href="#">5</a></li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">
-                          <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 6l6 6l-6 6"></path></svg>
-                        </a>
-                      </li>
+                    <h3 class="card-title">Section</h3>
+                    <ul class="steps steps-counter steps-vertical">
+
+                      <?php echo $list_step;?>
+                      <!-- <li class="step-item">Step one</li>
+                      <li class="step-item">Step two</li>
+                      <li class="step-item active">Step three</li>
+                      <li class="step-item">Step four</li>
+                      <li class="step-item">Step five</li>
+                      <li class="step-item">Step one</li>
+                      <li class="step-item">Step two</li>
+                      <li class="step-item active">Step three</li>
+                      <li class="step-item">Step four</li>
+                      <li class="step-item">Step five</li>
+                      <li class="step-item">Step one</li>
+                      <li class="step-item">Step two</li>
+                      <li class="step-item active">Step three</li>
+                      <li class="step-item">Step four</li>
+                      <li class="step-item">Step five</li>
+                      <li class="step-item">Step one</li>
+                      <li class="step-item">Step two</li>
+                      <li class="step-item active">Step three</li>
+                      <li class="step-item">Step four</li>
+                      <li class="step-item">Step five</li> -->
                     </ul>
                   </div>
                 </div>
               </div>
 
 
-
-<br>
-<br>
-<br>
-
-<!-- <div class="card">
-  <div class="card-body">
-    <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasStart" role="button" aria-controls="offcanvasStart">
-      Toggle start offcanvas
-    </a>
-    <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasEnd" role="button" aria-controls="offcanvasEnd">
-      Toggle end offcanvas
-    </a>
-    <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasTop" role="button" aria-controls="offcanvasTop">
-      Toggle top offcanvas
-    </a>
-    <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasBottom" role="button" aria-controls="offcanvasBottom">
-      Toggle bottom offcanvas
-    </a>
-  </div>
-</div> -->
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasStart" aria-labelledby="offcanvasStartLabel">
-  <div class="offcanvas-header">
-    <h2 class="offcanvas-title" id="offcanvasStartLabel">Start offcanvas</h2>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing 
-elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque 
-laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis 
-repellat sit soluta unde. Aut!
-    </div>
-    <div class="mt-3">
-      <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-        Close offcanvas
-      </button>
-    </div>
-  </div>
-</div>
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasEnd" aria-labelledby="offcanvasEndLabel">
-  <div class="offcanvas-header">
-    <h2 class="offcanvas-title" id="offcanvasEndLabel">End offcanvas</h2>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing 
-elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque 
-laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis 
-repellat sit soluta unde. Aut!
-    </div>
-    <div class="mt-3">
-      <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-        Close offcanvas
-      </button>
-    </div>
-  </div>
-</div>
-<div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-  <div class="offcanvas-header">
-    <h2 class="offcanvas-title" id="offcanvasTopLabel">Top offcanvas</h2>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing 
-elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque 
-laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis 
-repellat sit soluta unde. Aut!
-    </div>
-    <div class="mt-3">
-      <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-        Close offcanvas
-      </button>
-    </div>
-  </div>
-</div>
-<div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-  <div class="offcanvas-header">
-    <h2 class="offcanvas-title" id="offcanvasBottomLabel">Bottom offcanvas</h2>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing 
-elit. Ab assumenda ea est, eum exercitationem fugiat illum itaque 
-laboriosam magni necessitatibus, nemo nisi numquam quae reiciendis 
-repellat sit soluta unde. Aut!
-    </div>
-    <div class="mt-3">
-      <button class="btn btn-primary" type="button" data-bs-dismiss="offcanvas">
-        Close offcanvas
-      </button>
-    </div>
-  </div>
-</div>
-</div>
+              <div class="col-lg-8">
 
 
-    </div>
+                <div class="card">
+                  <div class="card-header bg-purple text-purple-fg">
+                    <h3 class="card-title "><?php echo $section_no.". ".$section_question;?></h3>
+                  </div>
+                  <div class="card-body">
+
+                  <form action="https://httpbin.org/post" method="post" class="card">
+
+
+
+
+                  
+                  <?php echo $list_question;?>
+
+
+
+
+                    </form> 
+
+
+                  </div>
+                </div>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+
     <?php include_once("php/html_footer.php");?>
   </div>
 </div>
